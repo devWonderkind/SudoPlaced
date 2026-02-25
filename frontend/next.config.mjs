33d055d485c64a9a7a2b5b56cd19e91ce5954806
Devang Shaurya Pratap Singh <devangshauryapratapsingh@gmail.com>
2026-02-25 22:13:05 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export', // This is the key setting
+  // Optional: If your images aren't loading, you may need to disable the default loader
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
