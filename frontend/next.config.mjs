3571d433f943ec457b60f9e9c89aad5f31c876fb
devWonderkind <akanshachahal758@gmail.com>
2026-03-11 12:51:04 +0530

    config: Enable Next.js static export output.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  // output: 'export',
+  output: 'export',
   // This is the key setting
   // Optional: If your images aren't loading, you may need to disable the default loader
   images: {
